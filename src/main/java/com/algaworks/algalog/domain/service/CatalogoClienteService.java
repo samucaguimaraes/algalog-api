aa5d4afbfbf95147f96e37ba90fab688ed0334e8
Samuel Guimarães <samuca.guimaraes@gmail.com>
2022-03-12 23:37:56 -0300

    Comite inicial

---
+++ src/main/java/com/algaworks/algalog/domain/service/CatalogoClienteService.java
@@ -0,0 +1,27 @@
+package com.algaworks.algalog.domain.service;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class CatalogoClienteService {
+
+	private ClienteRepository clienteRepository;
+	
+	@Transactional
+	public Cliente salvar(Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	@Transactional
+	public void excluir(Long clienteId) {
+		clienteRepository.deleteById(clienteId);
+	}
+}
