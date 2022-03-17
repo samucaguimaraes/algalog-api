193cde1a700695a5fc956699e26e1ae04f3f93a4
Samuel Guimarães <samuca.guimaraes@gmail.com>
2022-03-17 10:14:34 -0300

    add findByCliente()

--- src/main/java/com/algaworks/algalog/domain/repository/EntregaRepository.java
+++ src/main/java/com/algaworks/algalog/domain/repository/EntregaRepository.java
@@ -1,12 +1,15 @@
 package com.algaworks.algalog.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.model.Entrega;
 
 @Repository
 public interface EntregaRepository extends JpaRepository<Entrega, Long>{
 
-	
+	List<Entrega> findByCliente(Cliente cliente);
 }
